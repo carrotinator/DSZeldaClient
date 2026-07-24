e850e50efa0dd83f69f6741062e54a67a9ac8832
carrotinator <chrisb1820@gmail.com>
2026-07-24 12:18:07 +0200

    tloz-st 8.0: logic fixes, flag bug, heart overflow, extended hint location data

--- ItemClass.py
+++ ItemClass.py
@@ -175,6 +175,9 @@ async def remove_vanilla_normal(client: "DSZeldaClient", ctx: "BizHawkClientCont
         value = 9999 - prev_value if prev_value + value > 9999 else value
         value = prev_value if prev_value-value < 0 else value
     if "incremental" or "monotone_incremental" in item.tags:
+        if hasattr(item, "max") and prev_value >= item.max:
+            return []
+
         value = max(prev_value - value, 0)
     else:
         value = prev_value & (~value)
